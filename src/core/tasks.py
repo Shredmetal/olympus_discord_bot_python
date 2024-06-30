a27d90b0f4aa45f157c8cb890f9f459838aa5fab
Shredmetal <morganj.lee01@gmail.com>
2024-06-30 12:41:12 +0800

    changed bot thread deletion task to delete threads over 14 days old that start with support for

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -6,7 +6,7 @@ from .shared_state import thread_states
 async def cleanup_old_threads(bot):
     for guild in bot.guilds:
         for thread in guild.threads:
-            if thread.name.startswith("Support for") and (discord.utils.utcnow() - thread.created_at).days > 7:
+            if thread.name.startswith("Support for") and (discord.utils.utcnow() - thread.created_at).days > 14:
                 try:
                     await thread.delete()
                     thread_states.pop(thread.id, None)
