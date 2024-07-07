cfe0d479303f72601e60e301f0ab1b007f01db3d
Shredmetal <morganj.lee01@gmail.com>
2024-07-07 14:30:21 +0800

    There are a few places where bot needs to check if thread is closed. In which case, the same string will be passed. Moved that string to constants.py for D.R.Y.

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -6,3 +6,9 @@ INTENTS.message_content = True
 TROUBLESHOOTING_CHANNEL_ID = 1256834486419722341
 COMMUNITY_SUPPORT_CHANNEL_ID = 1256834429557542975
 PANTHEON_CHANNEL_ID = 1256855867718893568
+
+THREAD_CLOSED_STRING = ("This thread is no longer being monitored by the Olympus Discord Bot. Depending on your "
+                        "interactions above, this means you either need to follow the instructions and install "
+                        "/ reinstall DCS (Olympus), or a member of the DCS Olympus Team is looking into your "
+                        "issue. Please be patient, as all of us have day jobs that need doing, which is how "
+                        "DCS Olympus is kept completely free for you.")
