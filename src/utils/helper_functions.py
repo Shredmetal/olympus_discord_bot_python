e39fec7286fdb883be383d895f64a38bd5c59778
Shredmetal <morganj.lee01@gmail.com>
2024-07-14 15:47:38 +0800

    helper_functions.py fixed grammar in response message for missing_files == None

--- src/utils/helper_functions.py
+++ src/utils/helper_functions.py
@@ -20,7 +20,7 @@ def generate_response_message(missing_files: typing.List[str], troubleshooting_c
         return f"Please upload the required log files (Olympus_log.txt and dcs.log).\n{random_gif}"
     else:
         return (f"Thank you for attaching the logs! Someone from the DCS Olympus Team will assist you eventually. "
-                f"In the meantime, please check the {troubleshooting_channel_mention} channel make sure you have "
+                f"In the meantime, please check the {troubleshooting_channel_mention} channel, make sure you have "
                 f"followed the instructions there.")
 
 # TODO dcs.log check if the Olympus loading lines appear, In Olympus log file check for successful binding to the port
