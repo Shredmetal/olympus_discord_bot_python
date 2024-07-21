04841349e3fcee761e825ab699c809759288a648
Shredmetal <morganj.lee01@gmail.com>
2024-07-21 13:49:16 +0800

    refactored bot.py to use the new button views

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -4,7 +4,8 @@ import os
 from .commands import register_commands
 from .events import register_events
 from ..utils.constants import INTENTS
-from ..utils.views import LogOptionsView, ResolutionView
+from src.buttons.logs_views import InitialView
+from ..buttons.problem_resolution_view import ResolutionView
 
 bot = commands.Bot(command_prefix='/', intents=INTENTS)
 
@@ -15,7 +16,7 @@ register_events(bot)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    bot.add_view(LogOptionsView())
+    bot.add_view(InitialView())
     bot.add_view(ResolutionView())
 
 bot.run(os.environ['DISCORD_BOT_TOKEN'])
