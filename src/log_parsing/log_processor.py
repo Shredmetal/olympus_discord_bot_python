46ea0fcfecaf3a7d25e6f4df1e90fbc514bbe375
Shredmetal <morganj.lee01@gmail.com>
2024-07-23 22:39:25 +0800

    log_processor.py - implemented process_olympus_log function to return a specified string if access denied detected

--- src/log_parsing/log_processor.py
+++ src/log_parsing/log_processor.py
@@ -1,5 +1,5 @@
 import re
-from typing import List, Dict
+from typing import List, Dict, Union
 
 from discord import Attachment
 
@@ -23,14 +23,21 @@ def process_dcs_log(content: str) -> List[str]:
     return results
 
 
-def process_olympus_log(content: str) -> List[str]:
-    denied_mentions = re.findall(r'(?i).*denied.*', content)
-    results = []
-    # TODO: Apply processing logic to denied_mentions and populate results
-    return results
+def process_olympus_log(content: str) -> str:
+    access_denied_mentions = re.findall(r"Access denied: attempting to add Address", content)
+    if not access_denied_mentions:
+        return ""
+    else:
+        return ("Access denied detected in Olympus_log.txt - likely causes of this are: (1) You did not use netsh to "
+                "remove a URL reservation or (2) you picked to enable direct backend connections but did NOT do a URL "
+                "reservation to allow it to bind the port. This was specified in the installation instructions. Either "
+                "way, in DCS Olympus v1.0.4, we have removed the need to use the netsh spell in your command line "
+                "spellcasting interface, however, you will need to remove it if you have done so previously. Please "
+                "follow the instructions "
+                "[here](https://github.com/Pax1601/DCSOlympus/wiki#123-removing-the-net-shell-netsh-rule).")
 
 
-async def process_attachments(attachments: List[Attachment]) -> Dict[str, List[str]]:
+async def process_attachments(attachments: List[Attachment]) -> Dict[str, Union[List[str], str]]:
     log_contents = await download_attachments(attachments)
     results = {}
 
