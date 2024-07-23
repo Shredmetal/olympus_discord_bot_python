53b2a8070a09b8e1888384a318dd7ff80a2c5e8f
Shredmetal <morganj.lee01@gmail.com>
2024-07-23 23:18:47 +0800

    events.py - renamed pantheon channel id to support requests ID as it's where to throw the hey i got logs message

--- src/bot/events.py
+++ src/bot/events.py
@@ -4,7 +4,7 @@ from ..buttons.logs_received_views import LogsReceivedView
 from ..buttons.problem_resolution_view import ResolutionView
 from ..log_parsing.log_processor import process_attachments
 from ..utils.helper_functions import check_missing_files, generate_response_message
-from ..utils.constants import TROUBLESHOOTING_CHANNEL_ID, PANTHEON_CHANNEL_ID
+from ..utils.constants import TROUBLESHOOTING_CHANNEL_ID, SUPPORT_REQUESTS_ID
 from ..utils.enums import ThreadState
 from ..core.shared_state import get_thread_state, set_thread_state
 from ..core.tasks import cleanup_old_threads
@@ -82,10 +82,10 @@ def register_events(bot):
 
 
 async def notify_pantheon(bot, thread, user):
-    pantheon_channel = bot.get_channel(PANTHEON_CHANNEL_ID)
+    pantheon_channel = bot.get_channel(SUPPORT_REQUESTS_ID)
     if pantheon_channel:
         user_identifier = f"{user.name}#{user.discriminator}"
         await pantheon_channel.send(
             f"Support request received with logs at {thread.mention} from user: {user_identifier}")
     else:
-        print(f"OLYMPUS DEBUG: Could not find the Pantheon channel with ID {PANTHEON_CHANNEL_ID}")
+        print(f"OLYMPUS DEBUG: Could not find the Pantheon channel with ID {SUPPORT_REQUESTS_ID}")
