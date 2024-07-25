8f3f8302b613e2882401069405a4006f0ccc5505
Shredmetal <morganj.lee01@gmail.com>
2024-07-25 21:52:50 +0800

    dcs_log_handler.py - added regex finders for the process_dcs_log function, low parsing logic to be applied

--- src/log_parsing/dcs_log_handler.py
+++ src/log_parsing/dcs_log_handler.py
@@ -3,7 +3,14 @@ from typing import List
 
 
 def process_dcs_log(content: str) -> List[str]:
-    olympus_mentions = re.findall(r'(?i).*olympus.*', content, re.MULTILINE)
+    successful_install_pattern = (r'Olympus\.HOOKS\.LUA.*olympus\.dll loaded successfully|Olympus\.HOOKS\.LUA.*Olympus '
+                                  r'v.+? C\+\+ module callbacks registered correctly')
+    installation_process_pattern = (r'Olympus\.HOOKS\.LUA.*Executing OlympusHook\.lua|Olympus\.HOOKS\.LUA.*Loading '
+                                    r'olympus\.dll from|Olympus\.dll.*Instance location retrieved successfully|Olympus\
+                                    .dll.*Loading \.dlls from')
+
+    successful_install_matches = re.findall(successful_install_pattern, content, re.IGNORECASE | re.MULTILINE)
+    installation_process_matches = re.findall(installation_process_pattern, content, re.IGNORECASE | re.MULTILINE)
     results = []
     # TODO: Apply processing logic to olympus_mentions and populate results
     return results
