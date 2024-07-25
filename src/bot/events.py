a4766920c0c8bd870d259562114ad2f944d7e1bd
Morgan Lee <morgan.lee@synpulse.com>
2024-07-25 12:25:28 +0800

    events.py - refactored buttons into different pacakge

--- src/bot/events.py
+++ src/bot/events.py
@@ -1,6 +1,6 @@
 import discord
 
-from ..buttons.common_issues_view import CommonIssuesView
+from src.buttons.common_issues_buttons.common_issues_view import CommonIssuesView
 from ..buttons.problem_resolution_view import ResolutionView
 from ..log_parsing.log_processor import process_attachments
 from ..utils.helper_functions import check_missing_files, generate_response_message
@@ -43,6 +43,7 @@ def register_events(bot):
                         if not missing_files:
 
                             set_thread_state(thread_id, ThreadState.LOGS_RECEIVED)
+                            initial_view = create_base_view("initial")
                             await message.channel.send(response_message, view=CommonIssuesView())
 
                             analysis_results = await process_attachments(attachments)
