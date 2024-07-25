e35786c7797b8273f20893d97d2430957948b39e
Morgan Lee <morgan.lee@synpulse.com>
2024-07-25 17:48:39 +0800

    event_utils.py - refactored away the handling of have logs and only have dcs log away into event utils. Also moved notify support requests there

--- src/bot/events.py
+++ src/bot/events.py
@@ -1,11 +1,9 @@
 import discord
 
-from ..buttons.base_view import create_base_view
-from ..buttons.problem_resolution_view import ResolutionView
-from ..log_parsing.log_processor import process_attachments
-from ..utils.missing_file_checker_functions import check_missing_files, generate_response_message
-from ..utils.constants import TROUBLESHOOTING_CHANNEL_ID, SUPPORT_REQUESTS_ID
-from ..utils.enums import ThreadState
+from .events_utils.events_utils import handle_awaiting_logs, handle_no_olympus_logs
+from ..main_utils.missing_file_checker_functions import generate_response_message
+from ..main_utils.constants import TROUBLESHOOTING_CHANNEL_ID, SUPPORT_REQUESTS_ID
+from ..main_utils.enums import ThreadState
 from ..core.shared_state import get_thread_state, set_thread_state
 from ..core.tasks import cleanup_old_threads
 
@@ -33,45 +31,9 @@ def register_events(bot):
                 current_state = get_thread_state(thread_id)
 
                 if current_state == ThreadState.AWAITING_LOGS:
-                    if message.attachments:
-                        attachments = message.attachments
-                        required_files = ["Olympus_log.txt", "dcs.log"]
-                        missing_files = check_missing_files(attachments, required_files)
-                        response_message = generate_response_message(missing_files,
-                                                                     f"<#{TROUBLESHOOTING_CHANNEL_ID}>")
-
-                        if not missing_files:
-
-                            set_thread_state(thread_id, ThreadState.LOGS_RECEIVED)
-                            await message.channel.send(response_message, view=create_base_view("common_issues"))
-
-                            analysis_results = await process_attachments(attachments)
-
-                            for log_file, results in analysis_results.items():
-                                if log_file == "Olympus_log.txt":
-                                    if results:
-                                        for result in results:
-                                            await message.channel.send(result,
-                                                                       view=ResolutionView(
-                                                                           include_not_resolved_no_logs=False))
-                                    else:
-                                        await message.channel.send(f"No issues found in {log_file}")
-
-                                # TODO Implement the dcs.log parsing logic
-                                # elif log_file == "dcs.log":
-                                #     if result:
-                                #         await message.channel.send(f"Issues found in {log_file}:")
-                                #         for issue in result:
-                                #             await message.channel.send(issue)
-                                #     else:
-                                #         await message.channel.send(f"No issues found in {log_file}")
-
-                            await notify_pantheon(bot, message.channel, message.author)
-                        else:
-                            await message.channel.send(response_message)
-                    else:
-                        response_message = generate_response_message(None, f"<#{TROUBLESHOOTING_CHANNEL_ID}>")
-                        await message.channel.send(response_message)
+                    await handle_awaiting_logs(message, thread_id, current_state, bot)
+                elif current_state == ThreadState.NO_OLYMPUS_LOGS:
+                    await handle_no_olympus_logs(message, thread_id, current_state, bot)
                 elif current_state == ThreadState.LOGS_RECEIVED:
                     return
                 elif current_state == ThreadState.CLOSED:
@@ -82,13 +44,3 @@ def register_events(bot):
                     await message.channel.send(response_message)
 
         await bot.process_commands(message)
-
-
-async def notify_pantheon(bot, thread, user):
-    pantheon_channel = bot.get_channel(SUPPORT_REQUESTS_ID)
-    if pantheon_channel:
-        user_identifier = f"{user.name}#{user.discriminator}"
-        await pantheon_channel.send(
-            f"Support request received with logs at {thread.mention} from user: {user_identifier}")
-    else:
-        print(f"OLYMPUS DEBUG: Could not find the Pantheon channel with ID {SUPPORT_REQUESTS_ID}")
